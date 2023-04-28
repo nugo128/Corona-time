3fd001fc3334c8c7bf7bc27880573a55ebb53305
nugo128 <nugo.rostiashvili@redberry.ge>
2023-04-28 14:21:54 +0400

    style: dashboard header markup

---
+++ resources/views/components/dashboard-header.blade.php
@@ -0,0 +1,13 @@
+
+<div class="flex justify-between">
+<x-logo></x-logo>
+<div class="flex gap-3 items-center">
+    <x-dropdown></x-dropdown>
+    <h2>{{auth()->user()->username}}</h2>
+    <a href="#">Log Out</a>
+</div>
+</div>
+
+    <h2 class="font-bold mt-10">Worldwide Statistics</h2>
+
+
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,2 +1,10 @@
-<h1>dashboard!</h1>
-<p>{{auth()->user()->username}}</p>
+<x-layout>
+<div class="flex flex-col px-14 py-5">
+    <x-dashboard-header></x-dashboard-header>
+    <div class="flex mt-8 gap-10">
+        <div class="h-5"><p>Worldwide</p><div class="bg-black w-full h-1 mt-1"></div></div>
+        <div><p>By Country</p><span></span></div>
+    </div>
+</div>
+
+</x-layout>
